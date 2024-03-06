1a85f8c3ab77726c4b908cbad3bfb67f8f49fa7d
juandiazandreu4 <juanjrdiazandreu@gmail.com>
2024-03-06 11:39:21 +0100

    2-Función max_temp

--- practica4.js
+++ practica4.js
@@ -2,3 +2,20 @@ exports.load = (claseFilename) => {
     var valor = JSON.parse(claseFilename)
     return valor;
 }
+
+
+exports.max_temp = (cities) => {
+
+    if (cities.length === 0) {
+        return null; //por si detecta un cities vacio
+    }
+
+    let maximaTemperatura = cities[0].main.temp;
+    cities.forEach((city) => {
+        let temperaturaCiudad = city.main.temp;
+        if (temperaturaCiudad > maximaTemperatura){
+            maximaTemperatura = temperaturaCiudad;
+        }
+    });
+    return maximaTemperatura;
+}
